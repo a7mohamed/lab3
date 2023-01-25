b4425477359bdcbac7891bf079c0cc899507247e
a7mohamed <a7mohamed@ucsd.edu>
2023-01-25 12:27:04 -0800

    Update ArrayExamples.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,30 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
+  @Test
+  public void testReverseInPlace2(){
+    int[] input2 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input2);
+    //What I actually expect: {3, 2, 1}
+    assertArrayEquals(new int[]{3, 2, 1}, input2);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = {1, 2, 3};
+    //What I actually expect: {3, 2, 1}
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input2));
+  }
+  @Test
+  public void testReversed3() {
+    int[] input3 = {1, 2, 3, 4};
+    //What I actually expect: {4, 3, 2, 1}
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input3));
   }
 }
